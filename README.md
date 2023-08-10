d29683b9cecee65b9e924ea76135170555c58644
cshenton <charlieshenton@me.com>
2023-08-10 20:24:15 +1000

    basic build+print working

---
+++ README.md
@@ -0,0 +1,110 @@
+# Quest 2 Native Minimal
+
+This is a minimal repo to build an application for Quest 2 (Android, OpenXR) using:
+
+- No IDE
+- No build system
+- No scripting languages
+
+Just invocations of the C/C++ compiler and build tools that ship with the Android NDK.
+I also thoroughly document the provenance of every vendored and hidden dependency, so that you
+should be able to recreate the repo from scratch yourself. 
+
+## How to Build
+
+The build process depends on the following executables:
+
+- `keytool` in the Android JDK, used to generate a key store for signing
+- `jarsigner` in the Android JDK, used to sign our apk (instead of apksigner, since we're Android < 30)
+- `aarch64-linux-androidXX-clang` the clang cross-compiler that ships with the Android SDK (more specifically, the NDK)
+- `aapt` the Android Asset Packaging Tool which ships with the Android SDK
+- `zipalign` which is a zip alignment tool optimises the archive so it can be `mmap`ed, which ships with the Android SDK
+
+It also depends on various headers in the NDK, all specified below. The following `bash` code outlines the build process
+
+- Generate a key with `keytool`
+- Make a directory for the build
+- Compile our app as an `.so` with `aarch64-linux-androidXX-clang`
+- Copy our assets into the build directory, then package with `aapt`
+- Now unzip the generated `.apk` shove our `.so` files in there
+- Sign it with `jarsigner`
+- Align the zip archive with `zipalign`
+- Clean up
+
+```bash
+# Build tool, header, source, and library locations, you may need to change these
+JDK_KEYTOOL="C:/Program Files/Android/jdk/jdk-8.0.302.8-hotspot/jdk8u302-b08/bin/keytool.exe"
+JDK_JARSIGNER="C:/Program Files/Android/jdk/jdk-8.0.302.8-hotspot/jdk8u302-b08/bin/jarsigner.exe"
+ANDROID_SDK_HOME=C:/Users/User/AppData/Local/Android/Sdk
+ANDROID_JAR=$ANDROID_SDK_HOME/platforms/android-29/android.jar
+ANDROID_AAPT=$ANDROID_SDK_HOME/build-tools/34.0.0/aapt
+ANDROID_ZIPALIGN=$ANDROID_SDK_HOME/build-tools/34.0.0/zipalign
+
+ANDROID_NDK_HOME=$ANDROID_SDK_HOME/ndk/25.2.9519653
+ANDROID_LLVM=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/windows-x86_64
+ANDROID_CLANG=$ANDROID_LLVM/bin/aarch64-linux-android29-clang
+ANDROID_LIBS=$ANDROID_LLVM/sysroot/usr/include
+ANDROID_LIBS_LINK=$ANDROID_LLVM/sysroot/usr/lib/aarch64-linux-android/29
+
+# Once, generate your key with keytool. Don't delete this, or put it in version control!
+"$JDK_KEYTOOL" -genkey -v -keystore my-release-key.keystore -alias standkey -keyalg RSA -keysize 2048 -validity 10000 -storepass password -keypass password -dname "CN=example.com, OU=ID, O=Example, L=Doe, S=John, C=GB"
+
+# Make build directories
+mkdir -p build/assets
+mkdir -p build/lib/arm64-v8a
+
+# Compile binary
+$ANDROID_CLANG -ffunction-sections -Os -fdata-sections -Wall -fvisibility=hidden -I./src -Os -DANDROID -DAPPNAME=\"questxrexample\" -Ideps/include -I$ANDROID_LIBS -I-I$ANDROID_LIBS/android -fPIC -DANDROIDVERSION=29 -m64 -o build/lib/arm64-v8a/libquestxrexample.so src/main.cpp deps/src/android_native_app_glue.c deps/lib/libopenxr_loader.so -L$ANDROID_LIBS_LINK -Wl,--gc-sections -s -lm -lGLESv3 -lEGL -landroid -llog -shared -uANativeActivity_onCreate
+
+# Copy assets, binaries into build directory and package with aapt
+cp -r assets build
+cp deps/lib/libopenxr_loader.so build/lib/arm64-v8a/
+$ANDROID_AAPT package -f -F temp.apk -I $ANDROID_JAR -M src/AndroidManifest.xml -S resources -A build/assets -v --target-sdk-version 29
+
+# Unzip the apk back into the build directory to pick up our binaries, then sign and rezip it
+unzip -o temp.apk -d build
+rm -rf build.apk
+cd build && zip -D9r ../build.apk . && zip -D0r ../build.apk ./resources.arsc ./AndroidManifest.xml && cd ..
+"$JDK_JARSIGNER" -sigalg SHA1withRSA -digestalg SHA1 -verbose -keystore my-release-key.keystore -storepass password build.apk standkey
+$ANDROID_ZIPALIGN -f -v 4 build.apk questxrexample.apk
+
+# Clean up
+rm -rf temp.apk
+rm -rf build.apk
+rm -rf ./build
+```
+
+To run the build, plug in your developer mode enable quest 2. Then use `adb` to install and run the apk.
+
+```bash
+# Verify you're connected
+adb devices
+
+# Install the apk
+adb install -r questxrexample.apk
+
+# Run the apk
+adb shell am start -n org.cshenton.questxrexample/android.app.NativeActivity && adb logcat >> logs.txt
+```
+
+### A list of commands... that's basically just a rubbish build system!
+
+Yes that's the point, of _course_ you want some sort of build automation. But you probably
+want _your_ build automation. The point of these commands is to make it transparent so you
+can automate it for yourself! Whether that's using Make, CMake, or your own in-house build tool.
+
+## Vendor Provenance
+
+Okay, but where did the vendored dependencies come from?
+
+- From `ovr_openxr_mobile_sdk_55.0`
+  - `deps/include/openxr` was copied from `3rdParty/khronos/openxr/OpenXR-SDK/include`
+  - `deps/lib/libopenxr_loader.so` was copied from `OpenXR/Libs/Android/arm64-v8a/Release` 
+
+- From `github.com/cnlohr/tsopenxr`
+  - `deps/include/android_native_app_glue.h` was copied from `meta_quest/`
+  - `deps/src/android_native_app_glue.c` was copied from `meta_quest/`
+
+Why not just use the `android_native_app_glue` that ships with the NDK? Yeah good question. It didn't work, this one did.
+For reference the headers are the same (save a `#pragma once` vs a regular header guard), I need to properly bisect the
+source files to figure out the key difference.
---
+++ src/main.cpp
@@ -0,0 +1,242 @@
+// Editor Only, for now
+// #define ANDROID
+// #define EGLAPI
+// #define GL_APICALL
+
+#include "android_native_app_glue.h"
+#include <jni.h>
+#include <android/native_activity.h>
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdint.h>
+#include <EGL/egl.h>
+#include <GLES3/gl3.h>
+#include <android/log.h>
+
+#define XR_USE_PLATFORM_ANDROID
+#include "openxr/openxr.h"
+#include "openxr/openxr_platform.h"
+
+// System value set by the OS
+static int g_is_window_init;
+
+// Globals
+struct android_app *gapp;
+int android_width, android_height;
+int override_android_screen_dimensons = 0;
+int android_sdk_version;
+
+EGLNativeWindowType native_window;
+EGLDisplay egl_display;
+EGLSurface egl_surface;
+EGLContext egl_context;
+EGLConfig  egl_config;
+
+static EGLint const config_attribute_list[] = {
+	EGL_RED_SIZE, 8,
+	EGL_GREEN_SIZE, 8,
+	EGL_BLUE_SIZE, 8,
+	EGL_ALPHA_SIZE, 8,
+	EGL_BUFFER_SIZE, 32,
+	EGL_STENCIL_SIZE, 0,
+	EGL_DEPTH_SIZE, 16, // Maybe 32?
+	//EGL_SAMPLES, 1,
+	EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT,
+	EGL_NONE
+};
+
+extern "C" int __system_property_get( const char *__name, char *__value );
+
+extern "C" void handle_cmd( struct android_app *app, int32_t cmd )
+{
+	switch ( cmd )
+	{
+	case APP_CMD_DESTROY:
+		//This gets called initially after back.
+		// HandleDestroy();
+		ANativeActivity_finish( gapp->activity );
+		break;
+	case APP_CMD_INIT_WINDOW:
+		//When returning from a back button suspension, this isn't called.
+		if ( !g_is_window_init )
+		{
+			g_is_window_init = 1;
+			printf( "Got start event\n" );
+		}
+		else
+		{
+			// CNFGSetup( "", -1, -1 );
+			// HandleResume();
+		}
+		break;
+		//case APP_CMD_TERM_WINDOW:
+			//This gets called initially when you click "back"
+			//This also gets called when you are brought into standby.
+			//Not sure why - callbacks here seem to break stuff.
+		//	break;
+	default:
+		printf( "event not handled: %d", cmd );
+	}
+}
+
+
+extern "C" int32_t handle_input( struct android_app *app, AInputEvent *event )
+{
+	//Potentially do other things here.
+
+	// if ( AInputEvent_getType( event ) == AINPUT_EVENT_TYPE_MOTION )
+	// {
+	// 	int action = AMotionEvent_getAction( event );
+	// 	int whichsource = action >> 8;
+	// 	action &= AMOTION_EVENT_ACTION_MASK;
+	// 	size_t pointerCount = AMotionEvent_getPointerCount( event );
+
+	// 	for ( size_t i = 0; i < pointerCount; ++i )
+	// 	{
+	// 		int x, y, index;
+	// 		x = AMotionEvent_getX( event, i );
+	// 		y = AMotionEvent_getY( event, i );
+	// 		index = AMotionEvent_getPointerId( event, i );
+
+	// 		if ( action == AMOTION_EVENT_ACTION_POINTER_DOWN || action == AMOTION_EVENT_ACTION_DOWN )
+	// 		{
+	// 			int id = index;
+	// 			if ( action == AMOTION_EVENT_ACTION_POINTER_DOWN && id != whichsource ) continue;
+	// 			// HandleButton( x, y, id, 1 );
+	// 			ANativeActivity_showSoftInput( gapp->activity, ANATIVEACTIVITY_SHOW_SOFT_INPUT_FORCED );
+	// 		}
+	// 		else if ( action == AMOTION_EVENT_ACTION_POINTER_UP || action == AMOTION_EVENT_ACTION_UP || action == AMOTION_EVENT_ACTION_CANCEL )
+	// 		{
+	// 			int id = index;
+	// 			if ( action == AMOTION_EVENT_ACTION_POINTER_UP && id != whichsource ) continue;
+	// 			// HandleButton( x, y, id, 0 );
+	// 		}
+	// 		else if ( action == AMOTION_EVENT_ACTION_MOVE )
+	// 		{
+	// 			// HandleMotion( x, y, index );
+	// 		}
+	// 	}
+	// 	return 1;
+	// }
+	// else if ( AInputEvent_getType( event ) == AINPUT_EVENT_TYPE_KEY )
+	// {
+	// 	int code = AKeyEvent_getKeyCode( event );
+	// 	int unicode = AndroidGetUnicodeChar( code, AMotionEvent_getMetaState( event ) );
+	// 	if ( unicode ) {
+	// 		// HandleKey( unicode, AKeyEvent_getAction( event ) );
+		
+	// 	} else {
+	// 		// HandleKey( code, !AKeyEvent_getAction( event ) );
+	// 		return ( code == 4 ) ? 1 : 0; //don't override functionality.
+	// 	}
+	// 	return 1;
+	// }
+	return 0;
+}
+
+extern "C" void android_main(struct android_app *app) {
+	printf("Window: %p", app->window);
+	{
+		char sdk_ver_str[92];
+		int len = __system_property_get( "ro.build.version.sdk", sdk_ver_str );
+		if ( len <= 0 )
+			android_sdk_version = 0;
+		else
+			android_sdk_version = atoi( sdk_ver_str );
+	}
+
+	gapp = app;
+	app->onAppCmd = handle_cmd;
+	// app->onInputEvent = handle_input;
+	printf( "Starting with Android SDK Version: %d\n", android_sdk_version );
+
+ 	EGLint egl_major, egl_minor;
+	EGLint num_config;
+
+	int events;
+	while ( !g_is_window_init ) {
+		printf("Polling\n");
+		struct android_poll_source *source;
+		if ( ALooper_pollAll( 0, 0, &events, (void **)&source ) >= 0 )
+		{
+			if ( source != NULL ) source->process( gapp, source );
+		}
+	}
+	printf("Getting Display\n");
+
+	egl_display = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+
+	if ( egl_display == EGL_NO_DISPLAY ) {
+		printf( "Error: No display found!\n" );
+		return;
+	}
+
+	printf("Initialising EGL\n");
+	if ( !eglInitialize( egl_display, &egl_major, &egl_minor ) ) {
+		printf( "Error: eglInitialise failed!\n" );
+		return;
+	}
+
+	printf( "EGL Version: \"%s\"\n", eglQueryString( egl_display, EGL_VERSION ) );
+	printf( "EGL Vendor: \"%s\"\n", eglQueryString( egl_display, EGL_VENDOR ) );
+	printf( "EGL Extensions: \"%s\"\n", eglQueryString( egl_display, EGL_EXTENSIONS ) );
+
+	eglChooseConfig( egl_display, config_attribute_list, &egl_config, 1, &num_config );
+	printf( "Config: %d\n", num_config );
+
+	printf( "Creating Context\n" );
+        static const EGLint context_attribute_list[] = {
+                EGL_CONTEXT_CLIENT_VERSION, 2,
+                EGL_NONE
+        };
+	egl_context = eglCreateContext( egl_display, egl_config, EGL_NO_CONTEXT, context_attribute_list );
+	if ( egl_context == EGL_NO_CONTEXT ) {
+		printf( "Error: eglCreateContext failed: 0x%08X\n",
+				eglGetError() );
+		return;
+	}
+	printf( "Context Created %p\n", egl_context );
+
+
+	// Do I need this window code?
+	if ( native_window && !gapp->window ) {
+		printf( "WARNING: App restarted without a window.  Cannot progress.\n" );
+		return;
+	}
+
+	printf( "Getting Surface %p\n", native_window = gapp->window );
+
+	if ( !native_window ) {
+		printf( "FAULT: Cannot get window\n" );
+		return;
+	}
+
+        android_width = ANativeWindow_getWidth(native_window);
+        android_height = ANativeWindow_getHeight(native_window);
+
+	printf( "Width/Height: %dx%d\n", android_width, android_height );
+        static EGLint window_attribute_list[] = {
+                EGL_NONE
+        };
+	egl_surface = eglCreateWindowSurface( egl_display, egl_config, gapp->window, window_attribute_list );
+	printf( "Got Surface: %p\n", egl_surface );
+
+	if ( egl_surface == EGL_NO_SURFACE ) {
+		printf("Error: eglCreateWindowSurface failed: " "0x%08X\n", eglGetError() );
+		return;
+	}
+
+	if ( !eglMakeCurrent( egl_display, egl_surface, egl_surface, egl_context ) ) {
+		printf( "Error: eglMakeCurrent() failed: 0x%08X\n", eglGetError() );
+		return;
+	}
+
+	printf( "GL Vendor: \"%s\"\n", glGetString( GL_VENDOR ) );
+	printf( "GL Renderer: \"%s\"\n", glGetString( GL_RENDERER ) );
+	printf( "GL Version: \"%s\"\n", glGetString( GL_VERSION ) );
+	printf( "GL Extensions: \"%s\"\n", glGetString( GL_EXTENSIONS ) );
+
+	return;       
+}

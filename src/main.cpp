452c7714755895b1dfd4819b08ab7f64700d4e42
cshenton <charlieshenton@me.com>
2023-08-12 18:35:54 +1000

    coordinate system moment

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 #include <android/native_activity.h>
 
 #include <assert.h>
+#include <math.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <unistd.h>
@@ -58,6 +59,170 @@ static const EGLint context_attribute_list[] = {
 static EGLint window_attribute_list[] = {
         EGL_NONE
 };
+
+
+// Vertex Shader Program
+const char *vert_src = R"glsl(
+#version 320 es
+precision highp float;
+
+layout(location = 0) uniform mat4 mvp;
+layout(location = 0) out vec4 vc;
+
+void main() {
+        const vec3 cubeVertices[8] = vec3[8](
+                vec3(-0.1, -0.1, -0.1), // Vertex 0
+                vec3( 0.1, -0.1, -0.1), // Vertex 1
+                vec3( 0.1, -0.1,  0.1), // Vertex 2
+                vec3(-0.1, -0.1,  0.1), // Vertex 3
+                vec3(-0.1,  0.1, -0.1), // Vertex 4
+                vec3( 0.1,  0.1, -0.1), // Vertex 5
+                vec3( 0.1,  0.1,  0.1), // Vertex 6
+                vec3(-0.1,  0.1,  0.1)  // Vertex 7
+        );
+
+        const int cubeIndices[36] = int[36](
+                0, 2, 1, 0, 3, 2, // Bottom face
+                4, 5, 6, 4, 6, 7, // Top face
+                0, 1, 5, 0, 5, 4, // Front face
+                3, 6, 2, 3, 7, 6, // Back face
+                0, 4, 7, 0, 7, 3, // Left face
+                1, 2, 6, 1, 6, 5  // Right face
+        );
+
+        int index = clamp(gl_VertexID, 0, 35);
+        int element = clamp(cubeIndices[index], 0, 7);
+        vec3 pos = cubeVertices[element];
+
+        gl_Position = vec4(mvp * vec4(pos, 1.0));
+        vc = vec4(1, 0, 1, 1);
+}
+)glsl";
+
+
+// Fragement Shader Program
+const char *frag_src = R"glsl(
+#version 320 es
+precision highp float;
+
+layout(location = 0) in vec4 vc;
+layout(location = 0) out vec4 outColor;
+
+void main() {
+        outColor = vc;
+}
+)glsl";
+
+
+void matrix_from_pose(float* result, const XrPosef * pose) {
+	const float x2  = pose->orientation.x + pose->orientation.x;
+	const float y2  = pose->orientation.y + pose->orientation.y;
+	const float z2  = pose->orientation.z + pose->orientation.z;
+
+	const float xx2 = pose->orientation.x * x2;
+	const float yy2 = pose->orientation.y * y2;
+	const float zz2 = pose->orientation.z * z2;
+
+	const float yz2 = pose->orientation.y * z2;
+	const float wx2 = pose->orientation.w * x2;
+	const float xy2 = pose->orientation.x * y2;
+	const float wz2 = pose->orientation.w * z2;
+	const float xz2 = pose->orientation.x * z2;
+	const float wy2 = pose->orientation.w * y2;
+
+	result[0] = 1.0f - yy2 - zz2;
+	result[1] = xy2 + wz2;
+	result[2] = xz2 - wy2;
+	result[3] = 0.0f;
+
+	result[4] = xy2 - wz2;
+	result[5] = 1.0f - xx2 - zz2;
+	result[6] = yz2 + wx2;
+	result[7] = 0.0f;
+
+	result[8] = xz2 + wy2;
+	result[9] = yz2 - wx2;
+	result[10] = 1.0f - xx2 - yy2;
+	result[11] = 0.0f;
+
+	result[12] = pose->position.x;
+	result[13] = pose->position.y;
+	result[14] = pose->position.z;
+	result[15] = 1.0;
+}
+
+void matrix_mul(float* result, const float* a, const float* b) {
+	result[0] = a[0] * b[0] + a[4] * b[1] + a[8] * b[2] + a[12] * b[3];
+	result[1] = a[1] * b[0] + a[5] * b[1] + a[9] * b[2] + a[13] * b[3];
+	result[2] = a[2] * b[0] + a[6] * b[1] + a[10] * b[2] + a[14] * b[3];
+	result[3] = a[3] * b[0] + a[7] * b[1] + a[11] * b[2] + a[15] * b[3];
+
+	result[4] = a[0] * b[4] + a[4] * b[5] + a[8] * b[6] + a[12] * b[7];
+	result[5] = a[1] * b[4] + a[5] * b[5] + a[9] * b[6] + a[13] * b[7];
+	result[6] = a[2] * b[4] + a[6] * b[5] + a[10] * b[6] + a[14] * b[7];
+	result[7] = a[3] * b[4] + a[7] * b[5] + a[11] * b[6] + a[15] * b[7];
+
+	result[8] = a[0] * b[8] + a[4] * b[9] + a[8] * b[10] + a[12] * b[11];
+	result[9] = a[1] * b[8] + a[5] * b[9] + a[9] * b[10] + a[13] * b[11];
+	result[10] = a[2] * b[8] + a[6] * b[9] + a[10] * b[10] + a[14] * b[11];
+	result[11] = a[3] * b[8] + a[7] * b[9] + a[11] * b[10] + a[15] * b[11];
+
+	result[12] = a[0] * b[12] + a[4] * b[13] + a[8] * b[14] + a[12] * b[15];
+	result[13] = a[1] * b[12] + a[5] * b[13] + a[9] * b[14] + a[13] * b[15];
+	result[14] = a[2] * b[12] + a[6] * b[13] + a[10] * b[14] + a[14] * b[15];
+	result[15] = a[3] * b[12] + a[7] * b[13] + a[11] * b[14] + a[15] * b[15];
+}
+
+void matrix_invert(float* result, float* src) {
+	result[0] = src[0];
+	result[1] = src[4];
+	result[2] = src[8];
+	result[3] = 0.0f;
+	result[4] = src[1];
+	result[5] = src[5];
+	result[6] = src[9];
+	result[7] = 0.0f;
+	result[8] = src[2];
+	result[9] = src[6];
+	result[10] = src[10];
+	result[11] = 0.0f;
+	result[12] = -(src[0] * src[12] + src[1] * src[13] + src[2] * src[14]);
+	result[13] = -(src[4] * src[12] + src[5] * src[13] + src[6] * src[14]);
+	result[14] = -(src[8] * src[12] + src[9] * src[13] + src[10] * src[14]);
+	result[15] = 1.0f;
+}
+
+void matrix_proj_opengl(float *proj, float left, float right, float up, float down, float near, float far) {
+        assert(near < far);
+	const float tanAngleLeft = tan(left);
+	const float tanAngleRight = tan(right);
+	const float tanAngleDown = tan(up);
+	const float tanAngleUp = tan(down);
+
+	const float tanAngleWidth = tanAngleRight - tanAngleLeft;
+	const float tanAngleHeight = (tanAngleUp - tanAngleDown);
+	const float offset = near;
+
+        proj[0] = 2 / tanAngleWidth;
+        proj[4] = 0;
+        proj[8] = (tanAngleRight + tanAngleLeft) / tanAngleWidth;
+        proj[12] = 0;
+
+        proj[1] = 0;
+        proj[5] = 2 / tanAngleHeight;
+        proj[9] = (tanAngleUp + tanAngleDown) / tanAngleHeight;
+        proj[13] = 0;
+
+        proj[2] = 0;
+        proj[6] = 0;
+        proj[10] = -(far + offset) / (far - near);
+        proj[14] = -(far * (near + offset)) / (far - near);
+
+        proj[3] = 0;
+        proj[7] = 0;
+        proj[11] = -1;
+        proj[15] = 0;
+}
 
 extern "C" int __system_property_get(const char *__name, char *__value);
 
@@ -113,8 +278,8 @@ extern "C" void android_main(android_app *app) {
         while (!g_window_is_init) {
                 struct android_poll_source *source;
                 if ( ALooper_pollAll( 0, 0, &events, (void **)&source ) >= 0 ) {
-                        if (source != NULL) { 
-                                source->process(gapp, source); 
+                        if (source != NULL) {
+                                source->process(gapp, source);
                         }
                 }
         }
@@ -396,13 +561,13 @@ extern "C" void android_main(android_app *app) {
 	XrReferenceSpaceCreateInfo space_desc;
 	space_desc.type = XR_TYPE_REFERENCE_SPACE_CREATE_INFO;
 	space_desc.next = NULL;
-	space_desc.referenceSpaceType = XR_REFERENCE_SPACE_TYPE_STAGE;
+	space_desc.referenceSpaceType = XR_REFERENCE_SPACE_TYPE_VIEW;
 	space_desc.poseInReferenceSpace = identity_pose;
 
         XrSpace xr_stage_space;
 	result = xrCreateReferenceSpace(xr_session, &space_desc, &xr_stage_space);
         assert(XR_SUCCEEDED(result));
-        
+
         // Create Actions
 	XrActionSetCreateInfo action_set_desc;
 	action_set_desc.type = XR_TYPE_ACTION_SET_CREATE_INFO;
@@ -603,7 +768,7 @@ extern "C" void android_main(android_app *app) {
 
                 result = xrEnumerateSwapchainImages(xr_swapchains[i], 0, &xr_swapchain_lengths[i], NULL);
                 assert(XR_SUCCEEDED(result));
-		
+
 		for (int j = 0; j < xr_swapchain_lengths[i]; j++) {
 			xr_swapchain_images[i][j].type = XR_TYPE_SWAPCHAIN_IMAGE_OPENGL_ES_KHR;
 			xr_swapchain_images[i][j].next = NULL;
@@ -627,7 +792,7 @@ extern "C" void android_main(android_app *app) {
         uint32_t xr_colour_targets[8];
         uint32_t xr_depth_targets[8];
         uint32_t xr_framebuffers[8];
-        
+
         uint32_t xr_blit_framebuffer;
         glGenFramebuffers(1, &xr_blit_framebuffer);
 
@@ -643,12 +808,12 @@ extern "C" void android_main(android_app *app) {
                 glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, NULL);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
                 glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
-                glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, xr_colour_targets[i], 0);  
+                glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, xr_colour_targets[i], 0);
 
                 glGenTextures(1, &xr_depth_targets[i]);
                 glBindTexture(GL_TEXTURE_2D, xr_depth_targets[i]);
                 glTexImage2D(GL_TEXTURE_2D, 0, GL_DEPTH24_STENCIL8, width, height, 0, GL_DEPTH_STENCIL, GL_UNSIGNED_INT_24_8, NULL);
-                glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, xr_depth_targets[i], 0); 
+                glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, xr_depth_targets[i], 0);
 
                 uint32_t draw_bufs[1] = {GL_COLOR_ATTACHMENT0};
                 glDrawBuffers(1, draw_bufs);
@@ -656,6 +821,46 @@ extern "C" void android_main(android_app *app) {
                 assert(glCheckFramebufferStatus(GL_FRAMEBUFFER) == GL_FRAMEBUFFER_COMPLETE);
         }
 
+        // Compile Shader
+        uint32_t vert_shd = glCreateShader(GL_VERTEX_SHADER);
+        glShaderSource(vert_shd, 1, &vert_src, NULL);
+        glCompileShader(vert_shd);
+
+        int success;
+        glGetShaderiv(vert_shd, GL_COMPILE_STATUS, &success);
+        if (!success) {
+                GLchar infoLog[512];
+                glGetShaderInfoLog(vert_shd, 512, NULL, infoLog);
+                printf("Vertex shader compilation failed:\n %s\n", infoLog);
+        }
+
+        uint32_t frag_shd = glCreateShader(GL_FRAGMENT_SHADER);
+        glShaderSource(frag_shd, 1, &frag_src, NULL);
+        glCompileShader(frag_shd);
+
+        glGetShaderiv(frag_shd, GL_COMPILE_STATUS, &success);
+        if (!success) {
+                GLchar infoLog[512];
+                glGetShaderInfoLog(frag_shd, 512, NULL, infoLog);
+                printf("Fragment shader compilation failed:\n %s\n", infoLog);
+        }
+
+        uint32_t shader_prog = glCreateProgram();
+        glAttachShader(shader_prog, vert_shd);
+        glAttachShader(shader_prog, frag_shd);
+        glLinkProgram(shader_prog);
+
+        glGetProgramiv(shader_prog, GL_LINK_STATUS, &success);
+        if (!success) {
+                GLchar infoLog[512];
+                glGetProgramInfoLog(shader_prog, 512, NULL, infoLog);
+                printf("Program Linking failed:\n %s\n", infoLog);
+        }
+
+        glDeleteShader(vert_shd);
+        glDeleteShader(frag_shd);
+
+        // Loop State
         XrSessionState xr_session_state = XR_SESSION_STATE_UNKNOWN;
         bool is_running = true;
         bool is_session_ready = false;
@@ -758,7 +963,7 @@ extern "C" void android_main(android_app *app) {
                 result = xrSyncActions(xr_session, &action_sync_info);
                 assert(XR_SUCCEEDED(result));
 
-                // Render Frame
+                // Wait Frame
                 XrFrameState frame_state;
                 frame_state.type = XR_TYPE_FRAME_STATE;
                 frame_state.next = NULL;
@@ -769,6 +974,31 @@ extern "C" void android_main(android_app *app) {
 
                 result = xrWaitFrame(xr_session, &frame_wait, &frame_state);
                 assert(XR_SUCCEEDED(result));
+
+                // Get controller info
+                XrSpaceLocation hand_locations[2] = { { XR_TYPE_SPACE_LOCATION }, { XR_TYPE_SPACE_LOCATION } };
+		xrLocateSpace(xr_hand_spaces[0], xr_stage_space, frame_state.predictedDisplayTime, &hand_locations[0]);
+                xrLocateSpace(xr_hand_spaces[1], xr_stage_space, frame_state.predictedDisplayTime, &hand_locations[1]);
+
+		XrActionStateFloat trigger_states[2] = { { XR_TYPE_ACTION_STATE_FLOAT }, { XR_TYPE_ACTION_STATE_FLOAT } };
+		XrActionStateBoolean trigger_click_states[2] = { { XR_TYPE_ACTION_STATE_BOOLEAN }, { XR_TYPE_ACTION_STATE_BOOLEAN } };
+
+		XrActionStateGetInfo action_get_info = { XR_TYPE_ACTION_STATE_GET_INFO };
+		action_get_info.action = xr_trigger_action;
+		action_get_info.subactionPath = xr_hand_paths[0];
+		xrGetActionStateFloat(xr_session, &action_get_info, &trigger_states[0]);
+		action_get_info.subactionPath = xr_hand_paths[1];
+		xrGetActionStateFloat(xr_session, &action_get_info, &trigger_states[1]);
+		action_get_info.action = xr_trigger_click_action;
+		action_get_info.subactionPath = xr_hand_paths[0];
+		xrGetActionStateBoolean(xr_session, &action_get_info, &trigger_click_states[0]);
+		action_get_info.subactionPath = xr_hand_paths[1];
+		xrGetActionStateBoolean(xr_session, &action_get_info, &trigger_click_states[1]);
+
+                float hand_left_model[16];
+                float hand_right_model[16];
+                matrix_from_pose(hand_left_model, &hand_locations[0].pose);
+                matrix_from_pose(hand_right_model, &hand_locations[1].pose);
 
                 XrFrameBeginInfo frame_begin;
                 frame_begin.type = XR_TYPE_FRAME_BEGIN_INFO;
@@ -788,7 +1018,7 @@ extern "C" void android_main(android_app *app) {
                         views[i].type = XR_TYPE_VIEW;
                         views[i].next = NULL;
                 }
-                
+
                 uint32_t view_count_out;
                 XrViewState view_state = { XR_TYPE_VIEW_STATE };
 
@@ -801,6 +1031,7 @@ extern "C" void android_main(android_app *app) {
                 assert(XR_SUCCEEDED(result));
 
                 XrCompositionLayerProjectionView projection_layer_views[8]{};
+                float view_projection_matrices[8][16];
 
                 for (int i = 0; i < view_count_out; i++) {
                         projection_layer_views[i].type = XR_TYPE_COMPOSITION_LAYER_PROJECTION_VIEW;
@@ -812,6 +1043,21 @@ extern "C" void android_main(android_app *app) {
                         projection_layer_views[i].subImage.imageRect.extent.width = xr_swapchain_widths[i];
                         projection_layer_views[i].subImage.imageRect.extent.height = xr_swapchain_heights[i];
                         projection_layer_views[i].subImage.imageArrayIndex = 0;
+
+                        float left = projection_layer_views[i].fov.angleLeft;
+                        float right = projection_layer_views[i].fov.angleRight;
+                        float up = projection_layer_views[i].fov.angleUp;
+                        float down = projection_layer_views[i].fov.angleDown;
+
+                        float proj[16];
+                        matrix_proj_opengl(proj, left, right, up, down, 0.1, 100.0);
+
+                        float inv_view[16];
+                        float view[16];
+                        matrix_from_pose(inv_view, &projection_layer_views[i].pose);
+                        matrix_invert(view, inv_view);
+
+                        matrix_mul(&view_projection_matrices[i][0], proj, view);
                 }
 
                 if (frame_state.shouldRender) {
@@ -837,8 +1083,23 @@ extern "C" void android_main(android_app *app) {
                                 glFramebufferTexture2D(GL_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_TEXTURE_2D, colour_tex, 0);
                                 glFramebufferTexture2D(GL_FRAMEBUFFER, GL_DEPTH_ATTACHMENT, GL_TEXTURE_2D, xr_depth_targets[v], 0);
                                 glViewport(0, 0, width, height);
-                                glClearColor(v == 0 ? 1 : 0, v == 0 ? 1 : 0, v == 0 ? 0 : 1, 1);
+                                glClearColor(0.2, 0.2, 0.2, 1);
                                 glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+                                // Render Left Hand
+                                float left_mvp[16];
+                                matrix_mul(left_mvp, view_projection_matrices[v], hand_left_model);
+                                glUseProgram(shader_prog);
+                                // glUniformMatrix4fv(0, 1, GL_FALSE, left_mvp);
+                                glUniformMatrix4fv(0, 1, GL_FALSE, view_projection_matrices[v]);
+                                glDrawArrays(GL_TRIANGLES, 0, 36);
+
+                                // Render Right Hand
+                                float right_mvp[16];
+                                matrix_mul(right_mvp, view_projection_matrices[v], hand_right_model);
+                                glUseProgram(shader_prog);
+                                glUniformMatrix4fv(0, 1, GL_FALSE, right_mvp);
+                                glDrawArrays(GL_TRIANGLES, 0, 36);
 
                                 XrSwapchainImageReleaseInfo release_info = { XR_TYPE_SWAPCHAIN_IMAGE_RELEASE_INFO };
                                 result = xrReleaseSwapchainImage(xr_swapchains[v], &release_info);
@@ -871,7 +1132,7 @@ extern "C" void android_main(android_app *app) {
                 result = xrEndSession(xr_session);
                 assert(XR_SUCCEEDED(result));
         }
-	
+
         result = xrDestroySession(xr_session);
         assert(XR_SUCCEEDED(result));
 
